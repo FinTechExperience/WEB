6c7810a8affd9b4756c6e626cf920a71f8a2e0f4
joseluisrosasbaza <Bazno379!>
2015-04-12 03:11:50 -0500

    New sections

--- js/section-handler.js
+++ js/section-handler.js
@@ -5,3 +5,13 @@ function loadSavings(){
 function loadHome(){
   document.getElementById("container").innerHTML='<object type="text/html" data="home.html" ></object>';
 }
+
+function loadBanca(){
+  var obj = document.getElementById("container");
+  obj.innerHTML = '';
+  obj.innerHTML = '<object type="text/html" data="banca.html" ></object>';
+}
+
+function loadCredit(){
+   document.getElementById("container").innerHTML='<object type="text/html" data="credito.html" ></object>';
+}
